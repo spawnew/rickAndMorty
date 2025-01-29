2d374e005c38639340e7e551148730624cd67310
Lucas Sorzio <sorziolucas4@gmail.com>
2025-01-29 15:51:19 -0300

    consumo de api exitoso

---
+++ rick_and_morty/src/componentes/Obtenerpersonaje.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import Mostar from './Mostar';
+const Obtenerpersonaje = () => {
+    const [obtener, setObtener] = useState([])
+
+    const character = () => {
+        fetch("https://rickandmortyapi.com/api/character/?page=1")
+            .then((res) => res.json()) // Convierte  la respuesta a JSON no olvidar los () para la funcion json()
+            .then((data) => {
+                console.log(data.results);
+
+                setObtener(data.results);
+            })
+            .catch((error) => {
+                console.error('Error al obtener los datos del personaje:', error);
+            });
+    };
+
+
+
+    useEffect(() => {
+        character();
+    }, []);
+    return (
+        <div > <h1>personaje</h1>
+            {obtener.map((personaje) => < Mostar key={personaje.id } {...personaje} />)
+                 
+            }  
+        
+        </div>
+    )
+}
+
+export default Obtenerpersonaje;
---
+++ rick_and_morty/src/componentes/mostrarpersonajes/Mostrar.jsx
@@ -0,0 +1,13 @@
+
+
+const Mostrar = ({personaje}) => {
+  return (
+        <div >
+            <h1 > {name}</h1>
+            <img src={personaje.image} alt="" />
+        </div>
+}
+  )
+}
+
+export default Mostrar
