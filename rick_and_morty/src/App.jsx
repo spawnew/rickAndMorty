30df353b4081c89ba91fe13ea5138d65b119175b
Lucas Sorzio <sorziolucas4@gmail.com>
2025-07-30 22:30:19 -0300

    ,

--- rick_and_morty/src/App.jsx
+++ rick_and_morty/src/App.jsx
@@ -1,7 +1,7 @@
 
 import Obtenerpersonaje from './componentes/Obtenerpersonaje'
 import Detalle from './componentes/Detalle/Detalle'
-
+import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './componentes/Navbar/Navbar';
 function App() {
